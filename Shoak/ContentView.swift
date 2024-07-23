e6182d056107c29e1b130c7e290e249802c1b51a
chongin12 <chongin12@naver.com>
2024-07-23 15:57:11 +0900

    Initial Commit

---
+++ Shoak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shoak
+//
+//  Created by 정종인 on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shoak/ShoakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoakApp.swift
+//  Shoak
+//
+//  Created by 정종인 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
