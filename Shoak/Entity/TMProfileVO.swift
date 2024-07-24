a7e3db9910d83f5b3050f4b341624414f5729d43
Jeong Chong In <chongin12@naver.com>
2024-07-24 17:21:37 +0900

    [#4] TMProfileDTO, TMProfileVO 수정

--- Shoak/Entity/TMProfileVO.swift
+++ Shoak/Entity/TMProfileVO.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct TMProfileVO {
     var memberID: TMMemberID
-    var image: Image?
+    var imageURLString: String?
     var name: String
+}
+
+extension TMProfileVO {
+    static var mockData: TMProfileVO {
+        TMProfileVO(memberID: "TestData from VO",imageURLString: "https://picsum.photos/200/200", name: "TestName from VO")
+    }
 }
